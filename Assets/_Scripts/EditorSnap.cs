7276399fe6d446a59991b6c18635fbe3b1a564ac
Martyn <Martyn_Stewart89@hotmail.com>
2020-04-19 23:36:16 +0100

    4.129 Start of wapoint creation

---
+++ Assets/_Scripts/EditorSnap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    const float gridSnapSize = 10.2f;
+    private Waypoint waypoint;
+    private TextMesh label;
+    Vector3 gridPos;
+
+    void Start() {
+        label = transform.GetComponentInChildren<TextMesh>();
+    }
+
+    void Update() {
+
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid() {
+        
+        gridPos.x = Mathf.RoundToInt(transform.position.x / gridSnapSize) * gridSnapSize;
+        gridPos.y = Mathf.RoundToInt(transform.position.y / gridSnapSize) * gridSnapSize;
+        gridPos.z = Mathf.RoundToInt(transform.position.z / gridSnapSize) * gridSnapSize;
+        transform.position = gridPos;
+    }
+
+    private void UpdateLabel() {
+        string PosText = transform.position.x / gridSnapSize + "," + transform.position.z / gridSnapSize;
+        label.text = PosText;
+        transform.name = "Cube (" + PosText + ")";
+    }
+}
---
+++ Assets/_Scripts/EnemyMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    [SerializeField] float stepRate = 1f;
+
+    void Start() {
+
+        StartCoroutine(MoveWaypoint());
+        
+    }
+
+    IEnumerator MoveWaypoint() {
+        foreach (Waypoint step in path) {
+            Debug.Log(step.gameObject.name);
+            transform.position = step.transform.position;
+            yield return new WaitForSeconds(stepRate);
+        }
+        Debug.Log("Done");
+
+    }
+
+
+    void Update() {
+        
+    }
+}
